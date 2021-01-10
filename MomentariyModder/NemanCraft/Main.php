1967c7765c54aef8c3a946d2df87e191cc61bf64
Momentariy <59233803+Momentariy@users.noreply.github.com>
2021-01-10 16:20:38 +0300

    Update Main.php

--- MomentariyModder/NemanCraft/Main.php
+++ MomentariyModder/NemanCraft/Main.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace MomentariyModder\NemanCraft;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class Main extends PluginBase{
+
+    public function onEnable() : void{}
